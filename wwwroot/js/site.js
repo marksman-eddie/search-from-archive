4a3b16af0fa779b4348d3a3ef39205f4a213821b
Eugeniy Ivanov <eddiego1990@gmail.com>
2021-03-10 17:26:08 +0500

    значительная доработка модального окна в клиентской части

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,8 @@
 ﻿function getModal(id) {
-    let formGroup = document.getElementById('exampleFormControlSelect2');
+    let formGroup = document.getElementById('exampleFormControlSelect1');
     let modalName = document.getElementById('modalName');
     $(document).on('show.bs.modal', '.modal', function () {
-        debugger;
+        //debugger;
         formGroup.innerHTML += "";
         fetch('https://localhost:44380/popup/modalCode?idButton=' + id)
             .then(
@@ -23,12 +23,30 @@
             .catch(function (err) {
                 console.log('Fetch Error :-S', err);
             });
-    });
-    function show() {
-        $('.modal').modal('show');
-    }
-    show();
-    setTimeout(show, 6000);
+    });    
+        $('.modal').modal('show');    
+}
+
+function addInModal() {
+    // exampleFormControlSelect1 - id select
+    let selected = Array.from(exampleFormControlSelect1.options).filter(option => option.selected).map(option => option.value);
+    console.log(selected);
+    let output = document.getElementById('exampleFormControlSelect2');    
+    for (let i = 0; i < selected.length; i++) {
+        output.innerHTML += `<option>${selected[i]}</option>`;
+    }   
+}
+
+function clearOutput() {
+    let output = document.getElementById('exampleFormControlSelect2');
+    output.innerText += "";
+}
+
+function addToInput() {    
+    let selected = Array.from(exampleFormControlSelect2.options).map(option => option.value);
+    let inputForm = document.getElementById('1');
+    inputForm.value = selected;
+    $('.modal').modal('hide');
 }
 
 
